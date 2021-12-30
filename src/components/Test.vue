4311bdb5b2fd6bb48677e7aeacf5a1f6e05e2c6b
Shenhaoyi <shenhaoyi04@163.com>
2021-12-30 17:49:27 +0800

    test ref

---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="test">safass</div>
+</template>
+
+<script lang="ts">
+import vue, {ref} from 'vue';
+
+const count = ref(0);
+console.log(count);
+console.log(count.value);
+count.value++;
+console.log(count.value);
+export default {
+}
+</script>
+
+
+<style>
+.test {
+  width: 100%;
+  height: 100%;
+}
+</style>
