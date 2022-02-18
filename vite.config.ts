0c50a2fb8e8718058f8c3f4b1a1ae5b69610f645
Shenhaoyi <shenhaoyi04@163.com>
2022-02-18 10:54:38 +0800

    安装unplugin-auto-import, 自动导入vue hooks

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
+  // 自动引入组件 
   Components({
     // 搜索组件的目录
     dirs: ['src/components'],
@@ -14,6 +16,10 @@ export default defineConfig({
       ElementPlusResolver(),
     ],
     dts: true, // 是否生成components.d.ts文件，生成以支持组件的ts类型推断
+  }),
+  // 自动引入vue api
+  AutoImport({ /* options */
+    imports: ['vue', 'vue-router']
   })
   ],
   resolve: {
