a5d7fa4f7bbd49f9ff17a7a185825a84be0e8a02
Shenhaoyi <shenhaoyi04@163.com>
2022-07-08 11:33:53 +0800

    学习UseVModel原理及其使用

--- src/components/vueUse/index.vue
+++ src/components/vueUse/index.vue
@@ -2,9 +2,20 @@
   <UseConfirmDialog />
   <el-divider></el-divider>
   <UseEventBus />
+  <el-divider></el-divider>
+  <UseVModel v-model:name="formData.name" v-model:age="formData.age" v-model:sex="formData.sex" />
+  <div>
+    {{ formData }}
+  </div>
+
 </template>
 
 <script lang="ts" setup>
+const formData = reactive({
+  name: 'lily',
+  age: '8',
+  sex: 'boy'
+})
 </script>
 
 <style lang="less" scoped>
---
+++ src/components/vueUse/use-v-model.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    name:
+    <input v-model="_name"/>
+    age:
+    <input v-model="_age"/>
+    sex:
+    <input v-model="_sex"/>
+  </div>
+</template>
+<script lang="ts" setup>
+import { useVModel } from '@vueuse/core'
+const props = defineProps({
+  name: String,
+  age: String,
+  sex: String
+})
+const emit = defineEmits(['update:name', 'update:age', 'update:sex'])
+
+const _name = useVModel(props, 'name', emit)
+const _age = useVModel(props, 'age', emit)
+const _sex = useVModel(props, 'sex', emit)
+</script>
