22e08239a0953952d3400db028a508fa220cc780
Shenhaoyi <shenhaoyi04@163.com>
2024-04-03 12:33:41 +0800

    安装并配置vite-plugin-inspect

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,35 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import AutoImport from 'unplugin-auto-import/vite';
+import Inspect from 'vite-plugin-inspect';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-  // 自动引入组件 
-  Components({
-    // 搜索组件的目录
-    dirs: ['src/components'],
-    // element puls ui库解析器，也可以自定义
-    resolvers: [ // 自定义组件解析器
-      ElementPlusResolver(),
-    ],
-    dts: true, // 是否生成components.d.ts文件，生成以支持组件的ts类型推断
-  }),
-  // 自动引入vue api
-  AutoImport({ /* options */
-    imports: ['vue', 'vue-router']
-  })
+  plugins: [
+    vue(),
+    // 自动引入组件
+    Components({
+      // 搜索组件的目录
+      dirs: ['src/components'],
+      // element puls ui库解析器，也可以自定义
+      resolvers: [
+        // 自定义组件解析器
+        ElementPlusResolver(),
+      ],
+      dts: true, // 是否生成components.d.ts文件，生成以支持组件的ts类型推断
+    }),
+    // 自动引入vue api
+    AutoImport({
+      /* options */ imports: ['vue', 'vue-router'],
+    }),
+    // 查看中间产物
+    Inspect(),
   ],
   resolve: {
     alias: {
-      'vue': 'vue/dist/vue.esm-bundler.js' // 定义vue的别名，如果使用其他的插件，可能会用到别名
-    }
-  }
-})
+      vue: 'vue/dist/vue.esm-bundler.js', // 定义vue的别名，如果使用其他的插件，可能会用到别名
+    },
+  },
+});
